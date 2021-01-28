f69519d57477e52fcbacf9cee051f7ccddbde692
Razi Syed <admin@dotnetreport.com>
2021-01-28 00:26:54 -0600

    added number formatting, and corrected hide in detail option

--- wwwroot/js/dotnetreport.js
+++ wwwroot/js/dotnetreport.js
@@ -1287,6 +1287,14 @@ var reportViewModel = function (options) {
 					r.fontBold = col.fontBold;
 					r.fontColor = col.fontColor;
 					r.fieldId = col.fieldId;
+
+					if (self.decimalFormatTypes.indexOf(col.fieldFormat) >= 0) {
+						r.FormattedValue = self.formatNumber(r.Value, col.decimalPlaces);
+						switch (col.fieldFormat) {
+							case 'Currency': r.FormattedValue = '$' + r.FormattedValue; break;
+							case 'Percentage': r.FormattedValue = r.FormattedValue + '%'; break;
+                        }
+                    }
 				});
             }
 
@@ -1883,6 +1891,20 @@ var reportViewModel = function (options) {
 		self.ExecuteReportQuery(self.currentSql(), self.currentConnectKey(), self.ReportSeries);
 		return false;
 	};
+
+	self.formatNumber = function(number, decPlaces, decSep, thouSep) {
+		decPlaces = isNaN(decPlaces = Math.abs(decPlaces)) ? 2 : decPlaces,
+			decSep = typeof decSep === "undefined" ? "." : decSep;
+		thouSep = typeof thouSep === "undefined" ? "," : thouSep;
+		var sign = number < 0 ? "-" : "";
+		var i = String(parseInt(number = Math.abs(Number(number) || 0).toFixed(decPlaces)));
+		var j = (j = i.length) > 3 ? j % 3 : 0;
+
+		return sign +
+			(j ? i.substr(0, j) + thouSep : "") +
+			i.substr(j).replace(/(\decSep{3})(?=\decSep)/g, "$1" + thouSep) +
+			(decPlaces ? decSep + Math.abs(number - i).toFixed(decPlaces).slice(2) : "");
+	}
 
 	// ui-validation
 	self.isInputValid = function (ctl) {
